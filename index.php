872175ce6329f770060c306de8aa865205088d7c
Fernando Fernandez <ffmp777@yahoo.com>
2016-07-11 11:14:01 -0600

    PHP and HTML

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+    <style media="screen">
+      header {
+        background: #e3e3e3;
+        padding: 2em;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
 
+      <h1><?php
+//<?php echo is equivelent to <?=
 
-echo "Hello World!";
+      $name = $_GET['name']; //this will get the value from the URL parameter http://localhost:8888/?key=value
+
+      echo "Hello " . $name; //This can also be written like: echo "Hello, " . $_GET["name"]; or <?= "Hello, " . $_GET["name"];
+
+      ?></h1>
+<!-- PHPs closing tag ?> is necessary only when separating PHP code from other code, like in this example. -->
+
+    </header>
+  </body>
+</html>
---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+
+//variable
+$greeting = "Hello";
+$name = "Fernando";
+
+echo $greeting . " " . $name;
+//or
+echo 'Hello, ' . $name;
+//or
+echo "Hello $name";
+//The above will only work with "" and not with ''.
